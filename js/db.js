0496bd07a65bda13cc8a5e5632584c7b09b4a9a5
julienfty <dc6jz5vmpw@privaterelay.appleid.com>
2026-07-11 23:46:56 +0200

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,5 +1,8 @@
+// js/db.js
 const SUPABASE_URL = "https://hfrdzcjfienvegrofszz.supabase.co";
 const SUPABASE_KEY = "sb_publishable_U6qUMbeePU0ykN99-OQsNg_FGYTo0iZ";
 
-// On crée une variable globale 'supabaseClient'
+// On attache le client à la fenêtre (l'objet global)
 window.supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+console.log("Supabase est prêt !");
