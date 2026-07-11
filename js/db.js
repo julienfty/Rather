c024b1a3216d0345fa3d39c4b865632101ff2e7a
julienfty <dc6jz5vmpw@privaterelay.appleid.com>
2026-07-11 11:28:40 +0200

    Add files via upload

---
+++ js/db.js
@@ -0,0 +1,3 @@
+const SUPABASE_URL = "https://hfrdzcjfienvegrofszz.supabase.co";
+const SUPABASE_KEY = "sb_publishable_U6qUMbeePU0ykN99-OQsNg_FGYTo0iZ";
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
